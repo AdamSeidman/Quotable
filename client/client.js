a946979f640e539edc3e1e820b421703932d55bd
Adam Seidman <adamjseidman@gmail.com>
2023-05-13 20:42:31 -0400

    Fix client intents and add basic messaging

--- client/client.js
+++ client/client.js
@@ -8,12 +8,21 @@
 const { botToken } = require('./config')
 const Discord = require('discord.js')
 
+const db = require('../base/db')
+
 // Bot Intentions
-const myIntents = ['GuildMessages', 'MessageContent', 'GuildScheduledEvents']
+const myIntents = ['Guilds', 'GuildMessages', 'MessageContent', 'GuildScheduledEvents']
 
 const bot = new Discord.Client({ intents:myIntents })
 bot.login(botToken)
 
 bot.on('ready', () => {
+    db.setup()
     console.log('Quote Bot Initialized')
+})
+
+bot.on('messageCreate', msg => {
+    if (msg.member !== null && !msg.author.bot) {
+        db.addMessage(msg.content, msg.channel.id)
+    }
 })
