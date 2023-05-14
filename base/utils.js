af3309b3a2d4f268142dadf005cfd2e902dd18cd
Adam Seidman <adamjseidman@gmail.com>
2023-05-13 20:42:10 -0400

    Add common utilities to app

---
+++ base/utils.js
@@ -0,0 +1,22 @@
+/**
+ * Author: Adam Seidman
+ * 
+ * Useful utilities.
+ * 
+ * Exports:
+ *     copyObject-
+ *         Copy js obhect in memory to new object.
+ *     randomArrayItem-
+ *         Given an array as input, it will chose a random item as output. 
+ */
+
+var copyObject = function (obj) {
+    return JSON.parse(JSON.stringify(obj))
+}
+
+var randomArrayItem = function (arr) {
+    if (arr === undefined || arr.length <= 1) {
+        return arr
+    }
+    return arr[Math.floor(Math.random() * arr.length)]
+}
